2ee390e552c0d820e2e5a731d51a3dfcaffeabc5
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-12-13 07:31:58 -0300

    Criando Header

--- exemplo-redux/src/App.js
+++ exemplo-redux/src/App.js
@@ -3,11 +3,14 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 import GlobalStyle from './styles/global';
-import Routes from './routes'
+import Header from './components/Header';
+import Routes from './routes';
+
 
 function App() {
  return (
    <BrowserRouter>
+   <Header />
    <Routes />
    <GlobalStyle />
    </BrowserRouter>
