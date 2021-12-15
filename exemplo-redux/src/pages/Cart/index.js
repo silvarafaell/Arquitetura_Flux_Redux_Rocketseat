5747314baf1169d74b083c3826060c24741627d1
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-12-15 15:33:48 -0300

    Estilizacao do carrinho

--- exemplo-redux/src/pages/Cart/index.js
+++ exemplo-redux/src/pages/Cart/index.js
@@ -1,5 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <h1>Carrinho</h1>
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src='https://static.netshoes.com.br/produtos/tenis-nike-air-max-masculino/26/HZM-5168-026/HZM-5168-026_zoom2.jpg?ts=1623667246&ims=326x' alt='Tênis' />
+                        </td>
+                        <td>
+                            <strong>Tênis muito bom para correr rápido</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type='button'>
+                                    <MdRemoveCircleOutline size={20} color='#7159c1' />
+                                </button>
+                                <input type='number' readOnly value={2} />
+                                <button type='button'>
+                                    <MdAddCircleOutline size={20} color='#7159c1' />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type='button'>
+                                <MdDelete size={20} color='#7159c1' />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type='button'>Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920,20</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
