7fbaab43edcef3cd1bf927f1dc5550f60910096d
FRANCISCO RAFAEL PEREIRA DA SILVA <117311@pmenos.com.br>
2021-12-17 08:57:02 -0300

    Configurando o Redux

--- exemplo-redux/src/App.js
+++ exemplo-redux/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
 
 function App() {
- return (
-   <BrowserRouter>
-   <Header />
-   <Routes />
-   <GlobalStyle />
-   </BrowserRouter>
- )
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 export default App;
