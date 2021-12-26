542c223795f7cc7d9096eb4bcb10901a39273c24
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-12-25 22:48:49 -0300

    correcao router

--- exemplo-redux/src/App.js
+++ exemplo-redux/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
